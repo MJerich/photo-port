6b5d102a61d98f1c834e04141c3f936e19395944
MJerich <MatthewJerich@gmail.com>
2022-06-17 08:32:59 -0700

    added about test

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ import coverImage from "../../assets/cover/cover-image.jpg";
 function About() {
   return (
     <section>
-      <h1 id="about">Hack the Planet!</h1>
+      <h1 id="about">Who am I?</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     </section>
   );
