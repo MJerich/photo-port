cbdfcd6286783484b34a8db3f76e94d58bfddabe
MJerich <MatthewJerich@gmail.com>
2022-06-24 06:45:26 -0700

    finished

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import coverImage from "../../assets/cover/cover-image.jpg";
-
+import coverImage from '../../assets/cover/cover-image.jpg';
 function About() {
   return (
-    <section>
+    <section className="my-5">
       <h1 id="about">Who am I?</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Et malesuada fames ac turpis egestas integer. Lorem sed risus ultricies tristique nulla aliquet enim. 
-      Nunc id cursus metus aliquam eleifend mi in nulla posuere. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. 
-      Pretium nibh ipsum consequat nisl vel pretium lectus quam id. Etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum. 
-      Tempus quam pellentesque nec nam. Ipsum suspendisse ultrices gravida dictum fusce. Nam at lectus urna duis convallis. 
-      Accumsan in nisl nisi scelerisque. Pellentesque habitant morbi tristique senectus.
-      </p>
+      <div className="my-2">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
+          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
+          mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
+          Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
+          justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
+          dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
+          Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+        </p>
+      </div>
     </section>
   );
 }
